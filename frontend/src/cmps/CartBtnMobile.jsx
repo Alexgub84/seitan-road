6e31a985245755f11097929adc3f333a2ea4051e
Alexgub84 <alex.gub@gmail.com>
2021-04-01 19:19:04 +0300

    open cart btn design without functions

--- frontend/src/cmps/CartBtnMobile.jsx
+++ frontend/src/cmps/CartBtnMobile.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 export function CartBtnMobile() {
   return (
-    <div className="cart-toggle-mobile">
-      <img className="open" alt="" />
-      <div>סך קניות</div>
+    <div className="cart-toggle-mobile close">
+      <div className="img-container">
+        <img alt="" />
+      </div>
+      <div className="white-block">סל קניות</div>
     </div>
   );
 }
